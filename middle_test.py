12a2de7b0672dbf7de51975d41b881554939b704
kukunayida <kukunayida@163.com>
2015-10-22 10:28:56 +0800

    2015-10-22-10:17:26

---
+++ middle_test.py
@@ -0,0 +1,8 @@
+__author__ = 'JWLiu'
+
+# ≤‚ ‘doc_string
+import odbchelper
+
+#params = {"server": "mpilgrim", "database": "master", "uid": "sa", "pwd": "secret"}
+#print odbchelper.buildConnectionString(params)
+print odbchelper.buildConnectionString.__doc__
